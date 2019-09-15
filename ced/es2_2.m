69959e08cbb27956e06f901e6325a435046539ba
Probfia Gao <probfiagao@Probfias-MoneyBank-Pro.local>
2019-09-15 11:01:57 +0800

    updated ced

--- ced/es2_2.m
+++ ced/es2_2.m
@@ -1,5 +1,5 @@
-r = [-3:0.05:3];
-z = [0:0.05:4];
+r = [-5:0.05:5];
+z = [-4:0.05:4];
 n = length(r);
 m = length(z);
 phi = zeros(n,m);
@@ -7,7 +7,7 @@ for i = 1:n
     for j = 1:m
         x = r(i);
         y = z(j);
-        f = @(k) exp(-k.^2/2 - k.*y).*k.*besselj(0,k.*x);
+        f = @(k) exp(-k.^2/2 - k.*abs(y)).*k.*besselj(0,k.*x);
         phi_temp = integral(f,0,Inf);
         phi(i,j) = phi_temp;
     end
